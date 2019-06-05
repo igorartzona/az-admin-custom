c48133042acc2b6e180732b72d5a31ada2964907
igorartzona <igor.artzona@gmail.com>
2019-06-05 12:30:09 +0300

    1.0.0. Release

--- az-admin-custom.php
+++ az-admin-custom.php
@@ -5,7 +5,7 @@
  * Plugin Name:       az-admin-custom
  * Plugin URI:        https://github.com/igorartzona/az-admin-custom
  * Description:       Кастомизация админки
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            jvj
  * Author URI:        https://github.com/igorartzona 
  * License:           GPL-2.0+
@@ -20,22 +20,24 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**------Цветовой профиль-------------------------------------------------------**/
 
-add_action('admin_init', 'az_add_color_schemes');
+add_action( 'admin_init', 'az_add_color_schemes' );
 function az_add_color_schemes() { 
 	wp_admin_css_color(
 		'aztheme',
 		'artzona.org',
 		plugins_url( '/css/admin-colors.css', __FILE__ ),
-		array('#054765','#366482','#D16D15','#D46F15')
+		array( '#054765', '#366482', '#D16D15', '#D46F15' )
 	);
 }
 
 /*----------Логотип на странице входа-------------------*/
-add_action('login_head', 'az_add_login_logo');
+add_action( 'login_head', 'az_add_login_logo' );
 function az_add_login_logo() {
-	echo '<style type="text/css">
-			#login h1 a { 
-				background-image:url("'.plugins_url( "/img/logo.png", __FILE__ ).'"); 
-			}
+	?>
+	<style type="text/css">
+		#login h1 a { 
+			background-image:url("<?php echo plugins_url( "/img/logo.png", __FILE__ );?>"); 
+		}
 	</style>';
+	<?php
 }
